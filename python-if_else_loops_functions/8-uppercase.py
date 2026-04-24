ecc4c54f3446695f93d2881b254c8022ea52b413
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-24 14:26:36 +0000

    6-8 Uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for letter in str:
+        if ord(letter)<97:
+            print(letter, end="")
+        else:
+            print(chr(65+(ord(letter)-97)), end="")
