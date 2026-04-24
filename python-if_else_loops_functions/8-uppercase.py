b55b2dde8c1d1339ee2e0efe7825f0e888bc59e8
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-24 15:26:32 +0000

    manno

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str =""
-    for i in range (0,len(str)):
-        if ord(str[i])<97:
+    new_str = ""
+    for i in range(0, len(str)):
+        if ord(str[i]) < 97:
             new_str = new_str + str[i]
         else:
             new_str = new_str + chr(65+(ord(str[i])-97))
