b58c8564b80ea1be3cec3c6ebba378916128314c
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-24 20:38:44 +0000

    7-2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import sys 
-
+import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument:") 
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(count))
