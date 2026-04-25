c06d24008d45a0f1357519cf6e9fb28850132e7f
root <jessica.a.naegele@gmail.com>
2026-04-25 10:38:39 +0000

    7-6

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+    count = len(argv)
+
+    print(count)
+    print(argv[2])
+    print(argv)
+
+
+    if count < 4:
+        print("<a> <operator> <b>")
+        print("1")
+    elif argv[2] == "+":
+        a = int(argv[1])
+        b = int(argv[3])
+        c = add(a,b)
+        print("{} + {} = {}".format(a, b, c))
+    elif argv[2] == "-":
+        a = int(argv[1])
+        b = int(argv[3])
+        c = sub(a,b)
+        print("{} - {} = {}".format(a, b, c))
+    elif argv[2] == "*":
+        a = int(argv[1])
+        b = int(argv[3])
+        c = mul(a,b)
+        print("{} + {} = {}".format(a, b, c))
+    elif argv[2] == "/":
+        a = int(argv[1])
+        b = int(argv[3])
+        c = div(a,b)
+        print("{} + {} = {}".format(a, b, c))
+    else:
+        print("Unkown operator. Available operators: +, -, * and /")
+        print(1)
+    
+
+
+
+
+
