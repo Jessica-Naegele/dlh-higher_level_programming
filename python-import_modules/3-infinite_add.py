b504c321725917b19945972e2334d65d78f914c3
root <jessica.a.naegele@gmail.com>
2026-04-25 08:48:50 +0000

    7-3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv) 
+    int_agrv = [int(arg) for arg in sys.argv[1:]]
+    sum = 0
+    if count == 0:
+        print(sum)
+    else:
+        for i in range (0, count-1):
+            sum = sum + int_agrv[i]
+        print(sum)
+        
