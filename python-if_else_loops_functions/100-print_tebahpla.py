cc5b1ad379564829d6fac7181cc49c8986753ea3
root <jessica.a.naegele@gmail.com>
2026-04-25 13:24:58 +0000

    6-100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 26, 2):
-    print("{}{}".format(chr(122-i),chr(90-i-1)), end="")
-
-#zY
-# 1 print function
-# 1 loop
-# no characters stred in variable
-# no import
-#Großbuchstaben (A-Z): 65 bis 90
-#Kleinbuchstaben (a-z): 97 bis 122
+    print("{}{}".format(chr(122-i), chr(90-i-1)), end="")
