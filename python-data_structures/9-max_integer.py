aeca0fdc8ac0239fac9ce8b3e795647eae659818
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-28 13:15:18 +0000

    8-9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        j = -300000000000
+        for i in range(len(my_list)):
+            if my_list[i] > j:
+                j = my_list[i]
+        return j
