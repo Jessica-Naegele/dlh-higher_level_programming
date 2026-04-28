423c802f6ad60a0ddf31fb366fe272ef2f6283cd
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-28 18:34:35 +0000

    9-2: if len = 0

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add_list = [my_list[0]]
-    for i in my_list:
-        if i not in add_list:
-            add_list.append(i)
+    if len(my_list) == 0:
+        add_list = my_list
+    else:
+        add_list = [my_list[0]]
+        for i in my_list:
+            if i not in add_list:
+                add_list.append(i)
     r = sum(add_list)
     return r
