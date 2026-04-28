b25b4a81388b80f61f1a34889854b2ef52509309
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-28 10:10:00 +0000

    8-6 Iteration 2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 0:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[i])):
-                if j < len(matrix[i])-1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
-    else:
+    for row in matrix:
+        for j in range(len(row)):
+            print("{:d}".format(row[j]), end="")
+            if j < len(row) - 1:
+                print(" ", end="")
         print()
