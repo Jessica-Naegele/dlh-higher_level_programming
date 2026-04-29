60393f5c1ddd6ad7c47486c058ac97ca50f39eeb
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-29 09:07:46 +0000

    9-10 - 2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    elif a_dictionary is {}:
+    if not a_dictionary:
         return None
     else:
         return max(a_dictionary, key=a_dictionary.get)
