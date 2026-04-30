8e99c17d797e578ee14fc229a153de6a2bb98a97
=Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-30 10:14:34 +0000

    10-0 it2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,9 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print("")
         return x
+    except TypeError:
+        print("List is expected")
+        return 0
     except IndexError:
         j = 0
         for i in my_list:
@@ -12,3 +15,6 @@ def safe_print_list(my_list=[], x=0):
             print(i, end="")
         print("")
         return j
+    except KeyError:
+        print("List is expected")
+        return 0
