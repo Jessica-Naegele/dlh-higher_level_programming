fad0fdfe10a9a381e60cfa4bb55ec9eadcf3b47e
=Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-30 13:21:41 +0000

    10-1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    a = False
+    try:
+        value = int(value)
+        print("{:d}".format(value))
+        a = True
+    except (ValueError, TypeError):
+        pass
+    finally:
+        return a
