a823a6945702afe2b1e6206781d539259325ad7c
=Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-30 15:59:53 +0000

    11-3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    r = None
+    try:
+        r = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result {}".format(r))
