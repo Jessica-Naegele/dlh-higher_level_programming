91556e7b11647cbef532dae28d1eb2b7fdc4aae1
=Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-04-30 14:21:28 +0000

    11-2 it 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
-        if isinstance(my_list[i], int):
-            print("{}".format(my_list[i]), end="")
+        try:
+            print("{:d}".format(my_list[i]), end="")
             count += 1
+        except (ValueError, TypeError):
+            pass
     print("")
     return count
