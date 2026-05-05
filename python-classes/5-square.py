67035a6c8228b8321b25dfcf26ee3925dcbc4f4b
Jesscia-Naegele <jessica.a.naegele@gmail.com>
2026-05-05 14:54:25 +0000

    11-5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """This class square is defined by size and is delivering the area and my_print"""
+    """This class square is defined by size delivering the area and my_print"""
 
     def __init__(self, size=0):
         """Initializing - if no size is delivered default 0 will be used"""
@@ -13,7 +13,7 @@ class Square:
     def size(self):
         """getter property size"""
         return self.__size
-    
+
     @size.setter
     def size(self, size):
         """setter property is size"""
@@ -23,14 +23,14 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     def area(self):
         area = __size ** 2
         return area
-    
+
     def my_print(self):
         if self.__size == 0:
             print("")
-        else: 
-            for i in range (self.__size):
+        else:
+            for i in range(self.__size):
                 print("#" * self.__size)
