502c8a50e6d390d02f21adb3ba0230bb65965ce6
Jesscia-Naegele <jessica.a.naegele@gmail.com>
2026-05-05 15:43:21 +0000

    11-6

---
+++ python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""This is a module for creating class Square"""
+
+
+class Square:
+    """This class is calculating the area of a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializing for missing size and position default 0"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Getter Size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    @property
+    def position(self):
+        """Getter Property"""
+        return self.__position
+    
+    @position.setter
+    def position(self, position):
+        if isinstance(position, tuple) is not True or len(position) == 2:
+            if i in position < 0:     
+                raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            if self.__position[1] > 0:
+                for k in range(self.__position[1]):
+                    print("" * self.__position[1])
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
