91662c9383deb5f9a97c2c0a30661e8af2e0ef29
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-05-06 09:53:11 +0000

    12-1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module creates a class rectangle"""
+
+
+class Rectangle:
+    """This class is defined by height and width"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing default values"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        self.__width = width
+
+    @property
+    def height(self):
+        self.__height = height
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
