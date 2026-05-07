c6d15a96991c30ea2aaef125e6e66835888a3f4a
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-05-07 15:55:27 +0000

    3-3-7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,17 +2,17 @@
 """Add Item with 5 & 6"""
 
 # import sys and save_to_json_file and load_from_json_file
-import sys 
+import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import os
 
 if __name__ == "__main__":
-    # test whether file exists
-    if os.path.exists("add_item.json") == False:
+
+    if not os.path.exists("add_item.json"):
         # create a new file:
         with open("add_item.json", "w", encoding="utf-8") as f:
-            f.write("[]")  
+            f.write("[]")
     # append to file
     content = load_from_json_file("add_item.json")  # store existing content
     count = len(sys.argv) - 1
