94bbdd5f1f110babfee40ba3f694c9fb5a4840ae
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-05-07 10:18:00 +0000

    3-3 0 it 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """module reads a file and prints it to stout"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
