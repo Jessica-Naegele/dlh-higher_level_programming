4894016e686817f4385e4f708fbfc025f259c8ee
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-05-07 11:54:34 +0000

    3-2-1 it 2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8", newline='') as f:
         f.write(text)
+    return len(text)
