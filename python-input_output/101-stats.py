e10ac1ffac187a55118073a2ab264c443c22baf4
Jessica-Naegele <jessica.a.naegele@gmail.com>
2026-05-09 15:07:46 +0000

    3-3-101 it2

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -15,24 +15,25 @@ final_text = ()
 
 """ Input format: <IP Address> - [<date>]
 "GET /projects/260 HTTP/1.1" <status code> <file size>"""
-for line in sys.stdin:
-    parts = line.split()
-    # creates a list with every single aspect of the input
-    status_code = [-2]  # 2. letztes Attribut in der Liste
-    file_size = [-1]  # letztes Attribut in der liste
-    # add values to total_size and status
-    total_size += file_size
-    if status_code in status_counts:
-        status_counts[status_code] += 1
-    line_count += 1
-    # to enable printing after every 10th line
-    if line_count % 10 == 0:
-        # print must be inside the loop, otherwise, it will not print
-        for code in sorted(status.keys()):
-            if status[code] > 0:
-                final_text.append(status.key, status[code])
+if __name__ == "__main__":
+    for line in sys.stdin:
+        parts = line.split()
+        # creates a list with every single aspect of the input
+        status_code = [-2]  # 2. letztes Attribut in der Liste
+        file_size = [-1]  # letztes Attribut in der liste
+        # add values to total_size and status
+        total_size += file_size
+        if status_code in status_counts:
+            status_counts[status_code] += 1
+        line_count += 1
+        # to enable printing after every 10th line
+        if line_count % 10 == 0:
+            # print must be inside the loop, otherwise, it will not print
+            for code in sorted(status.keys()):
+                if status[code] > 0:
+                    final_text.append(status.key, status[code])
 
-print("File size: {}".format(total_size))
-for row in final_text:
-    for i, j in row:
-        print("{}: {}".format(i, j))
+    print("File size: {}".format(total_size))
+    for row in final_text:
+        for i, j in row:
+            print("{}: {}".format(i, j))
