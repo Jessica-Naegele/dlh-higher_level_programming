02203ec8d9de8638f2aabb01c8420e307b4ad773
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-27 15:08:05 +0200

    3-1-101

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This class is going to solve the n-Queen problems"""
+
+print(len('102'))
+print(len('1,5'))
+print(len('[1,2]'))
+print(len('4'))
+
+"""
+import sys
+
+N = sys.agrv
+
+def n_queens(N):
+    
+    if len(N)
+    l = []
+    for i in range(0,N)
+
+
+    
+    """
