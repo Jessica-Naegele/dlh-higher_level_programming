e60afc2af33889caa1ce8dd69389b68a4ae3c296
Jessie Naegele <jessica.a.naegele@gmail.com>
2026-05-27 14:04:15 +0200

    3-0-101

---
+++ python-classes/101-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""This is a module for creating class Square"""
+
+
+class Square:
+    """This class is calculating the area of a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializing for missing size and position default 0"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter Size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        """Getter Property"""
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if (
+            not isinstance(position, tuple) or len(position) != 2
+            or not all(isinstance(i, int) and i >= 0 for i in position)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        print(self)
+
+    def __str__(self):
+        """method to give print the same function as my_print"""
+        if self.__size == 0:
+            return ""
+        else:
+            lines = []
+            if self.__position[1] > 0:
+                for k in range(self.__position[1]):
+                    lines.append("")
+            for i in range(self.__size):
+                lines.append(" " * self.__position[0] + "#" * self.__size)
+        return "\n".join(lines)
